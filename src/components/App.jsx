0320b3db4b659a6fc3f203997b2ea4fbcab2858a
Andrii Krytsia <124679055+AndriiKrytsia@users.noreply.github.com>
2023-08-25 12:44:49 +0200

    jkgj

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,58 @@
-import { PhoneList } from '../components/PhoneList';
+import { Component } from 'react';
+import { ContactList } from './ContactList';
+import { ContactForm } from './ContactForm';
+import { Filter } from './Filter';
+import { nanoid } from 'nanoid';
 
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
-    >
-      <PhoneList />
-    </div>
-  );
-};
+export class App extends Component {
+  state = {
+    contacts: [],
+    filter: '',
+  };
+
+  handelAddContact = (name, number) => {
+    if (this.state.contacts.find(contact => contact.name === name)) {
+      alert(`${name} is already in contacts`);
+      return;
+    }
+    this.setState(prevState => ({
+      contacts: [...prevState.contacts, { id: nanoid(), name, number }],
+    }));
+  };
+
+  applyFilter = () => {
+    const { contacts, filter } = this.state;
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filter.toLowerCase())
+    );
+  };
+
+  handelChangeFilter = e => {
+    this.setState({ filter: e.target.value });
+  };
+
+  handelDeleteContact = id => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== id),
+    }));
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Phonebook</h1>
+        <ContactForm onAddContact={this.handelAddContact} />
+
+        <h2>Contacts</h2>
+        <Filter
+          onChangeFilter={this.handelChangeFilter}
+          filter={this.state.filter}
+        />
+        <ContactList
+          contacts={this.applyFilter()}
+          onDeleteContact={this.handelDeleteContact}
+        />
+      </div>
+    );
+  }
+}
